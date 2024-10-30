0440ddefc596f1c6df6f18908cd4eb9a7b35ad8a
James Griffiths <64775694+MIJOTHY-V2@users.noreply.github.com>
2024-10-30 09:00:24 +0000

    Revert dep removal for testing

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
 (defproject renovate-32177 "1.0.0-SNAPSHOT"
-  :dependencies [
+  :dependencies [[ch.qos.logback/logback-classic "1.5.11" :exclusions [org.slf4j/slf4j-api]]
                  [ch.qos.logback/logback-core "1.5.11"]]
   :description "Sample project")
